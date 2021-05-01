07f7b3152a90dcce03e5205c2401d63dc9ee0cb8
thomasrosen <thomas.rosen@me.com>
2021-05-01 14:24:09 +0200

    fetch user info

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,15 @@
+import { useEffect } from 'react'
 import classes from './App.module.css'
 
 function App() {
+
+  useEffect(()=>{
+    fetch('https://volt.link/user.json')
+      .then(response => response.json())
+      .then(data => console.log(data))
+      .catch(error => console.error(error))
+  }, [])
+
   return (<>
     <div className={classes.app}>
     </div>
