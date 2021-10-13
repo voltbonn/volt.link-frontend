9f9ce041cfeaea6b262227e40976ff80a0bd25ad
thomasrosen <thomas.rosen@me.com>
2021-10-13 13:12:43 +0200

    added left-header-actions to include a menu button

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,9 @@
 import classes from './Header.module.css'
 
-export default function Header({ title, rightActions, notificationBanner }) {
+export default function Header({ title, leftActions, rightActions, notificationBanner }) {
   return <header className={classes.header}>
     <div className={classes.headerBar}>
+      {leftActions}
       <h2>{title}</h2>
       {rightActions}
     </div>
