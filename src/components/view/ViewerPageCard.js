9f806a7527e1bd85c0419491d88f330a5b3f9169
thomasrosen <thomas.rosen@me.com>
2021-10-17 23:48:42 +0200

    added icons to the page viewer

--- src/components/view/ViewerPageCard.js
+++ src/components/view/ViewerPageCard.js
@@ -1,13 +1,26 @@
 import { useLocalization } from '../../fluent/Localized.js'
 
+import {
+  InsertDriveFile as PagePlaceholderIcon,
+} from '@mui/icons-material'
+
+import classes from './ViewerPageCard.module.css'
+
 function ViewerPageCard ({ block }) {
   const { fluentByAny, getString } = useLocalization()
 
+  const icon_url = block.properties.icon || ''
   const text = fluentByAny(block.properties.text, getString('placeholder_main_headline'))
   const description = fluentByAny(block.properties.description, '')
 
   return <div>
-    <h3 dir="auto">{text}</h3>
+    {
+      icon_url === ''
+        ? <PagePlaceholderIcon className={classes.icon} />
+        : <div className={classes.icon} style={{ backgroundImage: `url(${icon_url})` }} alt={text}></div>
+    }
+
+    <span dir="auto" className={classes.title}>{text}</span>
     {description !== '' ? <p dir="auto">{description}</p> : null}
   </div>
 }
