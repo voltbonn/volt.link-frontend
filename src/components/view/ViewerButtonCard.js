0f00cf5ae44ffa177aec41729f52425023505417
thomasrosen <thomas.rosen@me.com>
2021-10-17 22:34:27 +0200

    added onclick action to the button

--- src/components/view/ViewerButtonCard.js
+++ src/components/view/ViewerButtonCard.js
@@ -3,8 +3,42 @@ import { useLocalization } from '../../fluent/Localized.js'
 function ViewerButtonCard ({ block }) {
   const { fluentByAny } = useLocalization()
 
+  let url = ''
+  if (
+    block
+    && block.properties
+    && block.properties.trigger
+    && block.properties.action
+    && block.properties.action.url
+    && block.properties.trigger.type === 'click'
+    && block.properties.action.type === 'open_url'
+  ) {
+    url = block.properties.action.url
+  }
+
   const text = fluentByAny(block.properties.text, '')
-  return <button dir="auto" style={{ marginLeft: '0', marginRight: '0' }}>{text}</button>
+
+  if (url !== '') {
+    return <a href={url}>
+      <button
+        dir="auto"
+        style={{
+          marginLeft: '0',
+          marginRight: '0',
+        }}
+      >
+        {text}
+      </button>
+    </a>
+  }
+  return <button
+    dir="auto"
+    disabled="disabled"
+    style={{
+      marginLeft: '0',
+      marginRight: '0',
+    }}
+  >{text}</button>
 }
 
 export default ViewerButtonCard
