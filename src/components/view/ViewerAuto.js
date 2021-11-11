3a568b5b2ccfec1326e5c6ee85d4bcb9131b95d6
thomasrosen <thomas.rosen@me.com>
2021-11-11 15:06:56 +0100

    added page-line viewer

--- src/components/view/ViewerAuto.js
+++ src/components/view/ViewerAuto.js
@@ -5,16 +5,19 @@ import ViewerTextCard from './ViewerTextCard.js'
 import ViewerButtonCard from './ViewerButtonCard.js'
 import ViewerDividerCard from './ViewerDividerCard.js'
 import ViewerCheckboxCard from './ViewerCheckboxCard.js'
+import ViewerPageLine from './ViewerPageLine.js'
 import ViewerPageCard from './ViewerPageCard.js'
 import ViewerPersonCard from './ViewerPersonCard.js'
 import ViewerActionCard from './ViewerActionCard.js'
 
 import classes from './ViewerAuto.module.css'
 
-function ViewerAuto ({ block, actions, ...props }) {
+function ViewerAuto ({ block = {}, actions = {}, size = 'card', ...props }) {
   let component = null
 
-  switch (block.type) {
+  const type = block.type || null
+
+  switch (type) {
     case 'headline':
       component = <ViewerHeadlineCard key={block._id} block={block} actions={actions} {...props} />
       break
@@ -31,7 +34,13 @@ function ViewerAuto ({ block, actions, ...props }) {
       component = <ViewerCheckboxCard key={block._id} block={block} actions={actions} {...props} />
       break
     case 'page':
-      component = <ViewerPageCard key={block._id} block={block} actions={actions} {...props} />
+      switch (size) {
+        case 'line':
+          component = <ViewerPageLine key={block._id} block={block} actions={actions} {...props} />
+          break
+        default:
+          component = <ViewerPageCard key={block._id} block={block} actions={actions} {...props} />
+      }
       break
     case 'person':
       component = <ViewerPersonCard key={block._id} block={block} actions={actions} {...props} />
@@ -40,7 +49,7 @@ function ViewerAuto ({ block, actions, ...props }) {
       component = <ViewerActionCard key={block._id} block={block} actions={actions} {...props} />
       break
     default:
-      component = <ViewerTextCard key={block._id} block={block} actions={actions} {...props} />
+      component = JSON.stringify(block) // <ViewerTextCard key={block._id} block={block} actions={actions} {...props} />
   }
 
   return <div
---
+++ src/components/view/ViewerPageLine.js
@@ -0,0 +1,41 @@
+import { useCallback } from 'react'
+import { useNavigate } from 'react-router-dom'
+import { useLocalization } from '../../fluent/Localized.js'
+
+import {
+  InsertDriveFile as PagePlaceholderIcon,
+} from '@mui/icons-material'
+
+import classes from './ViewerPageLine.module.css'
+
+function ViewerPageLine ({ block, actions = {} }) {
+  const { fluentByAny, getString } = useLocalization()
+
+  let navigate = useNavigate()
+
+  const blockId = block._id
+
+  const viewBlock = useCallback(()=>{
+    navigate(`/view/${blockId}`)
+  }, [navigate, blockId])
+
+  const icon_url = block.properties.icon || ''
+  const text = fluentByAny(block.properties.text, getString('placeholder_main_headline'))
+
+  return <div
+    onClick={actions.hasOwnProperty('click') ? actions.click : viewBlock}
+    className={`clickable_card ${classes.root}`}
+  >
+    <div>
+      {
+        icon_url === ''
+          ? <PagePlaceholderIcon className={classes.icon} />
+          : <div className={classes.icon} style={{ backgroundImage: `url(${icon_url})` }} alt={text}></div>
+      }
+
+      <span dir="auto" className={classes.title}>{text}</span>
+    </div>
+  </div>
+}
+
+export default ViewerPageLine
