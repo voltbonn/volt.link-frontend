74dc5e381ee9f0af825b8ba998bd699215c1df4c
thomasrosen <thomas.rosen@me.com>
2021-11-15 20:22:57 +0100

    moved from loading user.json to graphQl user endpoint

--- src/hooks/useUser.js
+++ src/hooks/useUser.js
@@ -1,32 +1,28 @@
 import { useState, useEffect } from 'react'
 
+import { useApolloClient } from '@apollo/client'
+import { getSelf_Query } from '../graphql/queries'
+
 export default function useUser(forceRefetch = false) {
   const [user, setUser] = useState({})
   const [loggedIn, setLoggedIn] = useState(false)
+
+  const apollo_client = useApolloClient()
 
   useEffect(() => {
     if (forceRefetch !== true && window.hasOwnProperty('useUser_user') && window.hasOwnProperty('useUser_loggedIn')) {
       setUser(window.useUser_user)
       setLoggedIn(window.useUser_loggedIn)
     }else{
-      fetch(`${window.domains.backend}user.json`, {
-        mode: 'cors',
-        credentials: 'include',
+      apollo_client.query({
+        query: getSelf_Query,
       })
-        .then(response => response.json())
-        .then(data => {
-          if (
-            !!data
-            && !!data.user
-            && !!data.user.id
-            && !!data.user.status
-          ) {
-            data.user.username = (!!data.user.email ? data.user.email || '' : '').split('@')[0]
-
-            window.useUser_user = data.user
-            window.useUser_loggedIn = true
-            setUser(data.user)
-            setLoggedIn(true)
+        .then(async ({ data }) => {
+          if (data.hasOwnProperty('self')) {
+            window.useUser_user = data.self
+            window.useUser_loggedIn = data.self.logged_in
+            setUser(data.self)
+            setLoggedIn(data.self.logged_in)
           } else {
             window.useUser_user = {}
             window.useUser_loggedIn = false
@@ -34,9 +30,14 @@ export default function useUser(forceRefetch = false) {
             setLoggedIn(false)
           }
         })
-        .catch(error => console.error(error))
+        .catch(error => {
+          window.useUser_user = {}
+          window.useUser_loggedIn = false
+          setUser({})
+          setLoggedIn(false)
+        })
     }
-  }, [forceRefetch, setUser, setLoggedIn])
+  }, [forceRefetch, setUser, setLoggedIn, apollo_client])
 
   return {
     ...user,
