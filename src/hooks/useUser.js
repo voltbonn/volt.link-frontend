43581bfb85850196a3887286325af8cfbf457719
thomasrosen <thomas.rosen@me.com>
2021-11-16 17:24:36 +0100

    removed useUser cache
    Window wasn't a good option.
    Use something else instead.

--- src/hooks/useUser.js
+++ src/hooks/useUser.js
@@ -10,33 +10,23 @@ export default function useUser(forceRefetch = false) {
   const apollo_client = useApolloClient()
 
   useEffect(() => {
-    if (forceRefetch !== true && window.hasOwnProperty('useUser_user') && window.hasOwnProperty('useUser_loggedIn')) {
-      setUser(window.useUser_user)
-      setLoggedIn(window.useUser_loggedIn)
-    }else{
-      apollo_client.query({
-        query: getSelf_Query,
-      })
-        .then(async ({ data }) => {
-          if (data.hasOwnProperty('self')) {
-            window.useUser_user = data.self
-            window.useUser_loggedIn = data.self.logged_in
-            setUser(data.self)
-            setLoggedIn(data.self.logged_in)
-          } else {
-            window.useUser_user = {}
-            window.useUser_loggedIn = false
-            setUser({})
-            setLoggedIn(false)
-          }
-        })
-        .catch(error => {
-          window.useUser_user = {}
-          window.useUser_loggedIn = false
+    apollo_client.query({
+      query: getSelf_Query,
+    })
+      .then(async ({ data }) => {
+        if (data.hasOwnProperty('self')) {
+          setUser(data.self)
+          setLoggedIn(data.self.logged_in)
+        } else {
           setUser({})
           setLoggedIn(false)
-        })
-    }
+        }
+      })
+      .catch(error => {
+        console.error(error)
+        setUser({})
+        setLoggedIn(false)
+      })
   }, [forceRefetch, setUser, setLoggedIn, apollo_client])
 
   return {
