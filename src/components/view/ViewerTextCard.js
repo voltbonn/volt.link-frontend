3fe7d492a3096a451da3522e2db3f99c0438ee1f
thomasrosen <thomas.rosen@me.com>
2021-12-07 19:16:54 +0100

    BUGFIX correctly display whitespace in text

--- src/components/view/ViewerTextCard.js
+++ src/components/view/ViewerTextCard.js
@@ -1,5 +1,23 @@
+import { useState, useEffect } from 'react'
+
 function ViewerTextCard ({ block }) {
-  return <p dir="auto">{block.properties.text || ''}</p>
+  const [html, setHtml] = useState({ __html: '' })
+
+  useEffect(() => {
+    let text = block.properties.text || ''
+    text = text.replace(/\n/g, '<br>')
+    // text = text.replace(/\t/g, '&emsp;')
+
+    setHtml({ __html: text })
+  }, [block, setHtml])
+
+  return <p
+    dir="auto"
+    dangerouslySetInnerHTML={html}
+    style={{
+      'white-space': 'pre-wrap',
+    }}
+  ></p>
 }
 
 export default ViewerTextCard
