11f72534ed906a48f7b90a69427df97daec92d9c
thomasrosen <thomas.rosen@me.com>
2022-02-21 09:27:24 +0100

    removed deleteBlock mutation

--- src/graphql/mutations.js
+++ src/graphql/mutations.js
@@ -6,12 +6,6 @@ export const saveBlock_Mutation = gql`
   }
 `
 
-export const deleteBlock_Mutation = gql`
-  mutation deleteBlock ($_id: ObjectID!) {
-    deleteBlock (_id: $_id)
-  }
-`
-
 export const moveBlock_Mutation = gql`
   mutation moveBlock ($movingBlockId: ObjectID!, $newParentId: ObjectID!, $newIndex: Int!) {
     moveBlock(movingBlockId: $movingBlockId, newParentId: $newParentId, newIndex: $newIndex)
