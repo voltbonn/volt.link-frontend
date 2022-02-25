3c1679320e48b66c891ab9ee4dac7b52c0df188c
thomasrosen <thomas.rosen@me.com>
2022-02-26 00:14:03 +0100

    BUGFIX set the class for the buttons in error pages

--- src/components/ErrorPages.js
+++ src/components/ErrorPages.js
@@ -9,7 +9,7 @@ export default function LoginScreen () {
   return <>
     <h1>Login to view content</h1>
     <a href={`${window.domains.backend}login?redirect_to=${encodeURIComponent(window.location.toString())}`}>
-      <button className="hasIcon" style={{ margin: 'var(--basis_x4) 0 0 0' }}>
+      <button className="text hasIcon" style={{ margin: 'var(--basis_x4) 0 0 0' }}>
         <LoginIcon className="icon" />
         <span>
           <Localized id="login" />
@@ -36,7 +36,7 @@ export function ErrorPage ({ errorName }) {
     </>
 
     buttons = <a href={`${window.domains.backend}login?redirect_to=${encodeURIComponent(window.location.toString())}`}>
-      <button className="hasIcon" style={{ margin: 'var(--basis_x4) 0 0 0' }}>
+      <button className="text hasIcon" style={{ margin: 'var(--basis_x4) 0 0 0' }}>
         <LoginIcon className="icon" />
         <span>
           <Localized id="login" />
@@ -48,7 +48,7 @@ export function ErrorPage ({ errorName }) {
     description = "The URL you requested does currently not exist. Did you type the URL correctly?"
 
     buttons = <a href={`${window.domains.backend}login?redirect_to=${encodeURIComponent(window.location.toString())}`}>
-      <button className="hasIcon" style={{ margin: 'var(--basis_x4) 0 0 0' }}>
+      <button className="text hasIcon" style={{ margin: 'var(--basis_x4) 0 0 0' }}>
         <LoginIcon className="icon" />
         <span>
           <Localized id="login" />
