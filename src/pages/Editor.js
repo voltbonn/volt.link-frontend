c981b1694c3f9d32b3cefff4d8db78c5eb92d551
thomasrosen <thomas.rosen@me.com>
2022-03-02 12:39:57 +0100

    splited block-loading and the real editor into sub-components

--- src/pages/Editor.js
+++ src/pages/Editor.js
@@ -1,67 +1,22 @@
-import { useRef, useState, useCallback, useEffect } from 'react'
+import { useRef, useState, useEffect } from 'react'
 
 import {
   useParams,
 } from 'react-router-dom'
 
-import useSaveBlock from '../hooks/useSaveBlock.js'
+import classes from './Editor.module.css'
+
 import useLoadBlock from '../hooks/useLoadBlock.js'
 import useBlockMatchesRoles from '../hooks/useBlockMatchesRoles.js'
 
-// import Select from 'react-select'
-
-import {
-  // TranslateSharp as TranslateIcon,
-  IosShareSharp as ShareIcon,
-  LockSharp as PermissionsIcon,
-  SaveSharp as SaveIcon,
-  MoreHorizSharp as BlockMenuIcon,
-} from '@mui/icons-material'
-
-import classes from './Editor.module.css'
-
-import { Localized, useLocalization } from '../fluent/Localized.js'
-// import { useTranslatedInputContext, LocalesMenu } from '../components/edit/TranslatedInput.js'
-
 import Header from '../components/Header.js'
-import BlockMenu from '../components/edit/BlockMenu.js'
-import SharingEditor from '../components/edit/SharingEditor.js'
-import PermissionsEditor from '../components/edit/PermissionsEditor.js'
-import PropertiesEditor from '../components/edit/PropertiesEditor.js'
-import ContentEditor from '../components/edit/ContentEditor.js'
 import { ErrorPage } from '../components/ErrorPages.js'
 
-// const custom_react_select_styles = {
-//   menu: (provided, state) => ({
-//     ...provided,
-//     background: 'var(--purple)',
-//   }),
-// }
-// const custom_react_select_theme = theme => ({
-//   ...theme,
-//   borderRadius: 0,
-//   colors: {
-//     ...theme.colors,
-//     primary: 'var(--green)',
-//     primary25: 'var(--purple-dark)',
-//   },
-// })
+import PageEditor from '../components/apps/PageEditor.js'
+import AutomationEditor from '../components/apps/AutomationEditor.js'
 
 function Editor() {
-  const { getString } = useLocalization()
-
-  const saveBlock = useSaveBlock()
   const loadBlock = useLoadBlock()
-
-  // const { currentLocale, setLocales } = useTranslatedInputContext()
-
-  const [ isSharingEditorOpen, setIsSharingEditorOpen ] = useState(false)
-  const openSharingEditor = useCallback(() => setIsSharingEditorOpen(true), [ setIsSharingEditorOpen ])
-  const closeSharingEditor = useCallback(() => setIsSharingEditorOpen(false), [ setIsSharingEditorOpen ])
-
-  const [ isPermissionsEditorOpen, setIsPermissionsEditorOpen ] = useState(false)
-  const openPermissionsEditor = useCallback(() => setIsPermissionsEditorOpen(true), [ setIsPermissionsEditorOpen ])
-  const closePermissionsEditor = useCallback(() => setIsPermissionsEditorOpen(false), [ setIsPermissionsEditorOpen ])
 
   const loadedTheBlock = useRef(false)
   let { id = '' } = useParams()
@@ -71,10 +26,8 @@ function Editor() {
     properties: {},
     content: [],
   })
-  const type = block.type
-  const properties = block.properties
-  const content = block.content
-  const permissions = block.permissions
+
+  const type = block.type || 'page'
 
   const [ canEdit, setCanEdit ] = useState(null)
   const blockMatchesRoles = useBlockMatchesRoles()
@@ -115,71 +68,6 @@ function Editor() {
     // setLocales,
   ])
 
-  const saveType = useCallback(newType => {
-    if (newType !== block.type) {
-      const newBlock = {
-        ...block,
-        type: newType,
-      }
-
-      saveBlock(newBlock)
-        .then(gottenBlock => {
-          setBlock(gottenBlock)
-        })
-    }
-  }, [block, saveBlock, setBlock])
-
-  const saveProperties = useCallback(newProperties => {
-    if (newProperties !== block.properties) {
-      const newBlock = {
-        ...block,
-        properties: newProperties,
-      }
-
-      saveBlock(newBlock)
-        .then(gottenBlock => {
-          setBlock(gottenBlock)
-        })
-    }
-  }, [block, saveBlock, setBlock])
-
-  const saveContent = useCallback(newContent => {
-    if (newContent !== block.content) {
-      const newBlock = {
-        ...block,
-        content: newContent,
-      }
-
-      saveBlock(newBlock)
-        .then(gottenBlock => {
-          setBlock(gottenBlock)
-        })
-    }
-  }, [block, saveBlock, setBlock])
-
-  const savePermissions = useCallback(newPermissions => {
-    if (newPermissions !== block.permissions) {
-      const newBlock = {
-        ...block,
-        permissions: newPermissions,
-      }
-
-      saveBlock(newBlock)
-        .then(gottenBlock => {
-          setBlock(gottenBlock)
-        })
-    }
-  }, [block, saveBlock, setBlock])
-
-  const manuallySaveEverything = useCallback(() => {
-    saveBlock(block)
-      .then(gottenBlock => {
-        if (gottenBlock._id !== block._id) {
-          setBlock(gottenBlock)
-        }
-      })
-  }, [saveBlock, block, setBlock])
-
   // const isFirstRun = useRef(true)
   // useEffect(() => {
   //   if (isFirstRun.current) {
@@ -208,98 +96,17 @@ function Editor() {
       </div>
     </>
   } else if (canEdit === true) {
-
-  const rightHeaderActions = <div className="buttonRow" style={{ whiteSpace: 'nowrap' }}>
-    {/* <button className="text"><Localized id="path_editor_share"/></button> */}
-
-
-    {/*
-    <LocalesMenu
-      trigger={triggerProps => (
-        <button {...triggerProps} className="text hasIcon">
-          <TranslateIcon className="icon" />
-          <span className="hideOnSmallScreen" style={{verticalAlign: 'middle'}}>
-            <Localized id="path_editor_translate" />
-            {getString('locale_'+currentLocale)}
-          </span>
-        </button>
-      )}
-    />
-    */}
-
-    {
-      isSharingEditorOpen
-      ? <SharingEditor
-          defaultBlock={block}
-          open={isSharingEditorOpen}
-          onClose={closeSharingEditor}
-          onChange={saveProperties}
-        />
-      : null
+    if (type === 'automation') {
+      return <AutomationEditor
+        block={block}
+        onSaveBlock={setBlock}
+      />
+    } else {
+      return <PageEditor
+        block={block}
+        onSaveBlock={setBlock}
+      />
     }
-    <button className="text hasIcon" onClick={openSharingEditor}>
-      <ShareIcon className="icon" />
-      <span className="hideOnSmallScreen" style={{verticalAlign: 'middle'}}><Localized id="path_editor_share" /></span>
-    </button>
-
-    {
-      isPermissionsEditorOpen
-      ? <PermissionsEditor
-          defaultPermissions={permissions}
-          open={isPermissionsEditorOpen}
-          onClose={closePermissionsEditor}
-          onChange={savePermissions}
-        />
-      : null
-    }
-    <button className="text hasIcon" onClick={openPermissionsEditor}>
-      <PermissionsIcon className="icon" />
-      <span className="hideOnSmallScreen" style={{verticalAlign: 'middle'}}><Localized id="path_editor_permissions" /></span>
-    </button>
-
-    <button className="text hasIcon" onClick={manuallySaveEverything}>
-      <SaveIcon className="icon" />
-      <span className="hideOnSmallScreen" style={{verticalAlign: 'middle'}}><Localized id="path_editor_publish" /></span>
-    </button>
-
-    <BlockMenu
-      {...{
-        block,
-        setType: saveType,
-      }}
-
-      trigger={props => (
-        <button
-          {...props}
-          className="default hasIcon"
-        >
-          <BlockMenuIcon className="icon" />
-        </button>
-      )}
-    />
-  </div>
-
-  const title = block.properties.text || getString('placeholder_main_headline')
-
-  return <div key={block._id} className={`hasHeader ${classes.editor}`}>
-    <Header
-      block={block}
-      // title={<a href={`https://volt.link/${slug}`} target="_blank" rel="noopener noreferrer"><span className="hideOnSmallScreen">volt.link</span>/{slug}</a>}
-      title={title}
-      rightActions={rightHeaderActions}
-    />
-
-    <PropertiesEditor
-      type={type}
-      defaultProperties={properties}
-      onChange={saveProperties}
-    />
-
-    <ContentEditor
-      onChange={saveContent}
-      defaultValue={content}
-    />
-  </div>
   } else {
     return null
   }
