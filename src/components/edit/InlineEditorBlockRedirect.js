2ac8665abf01c961972f54093749833e1f7322f1
thomasrosen <thomas.rosen@me.com>
2022-03-20 23:44:32 +0100

    contrastingColor() is not needed in InlineEditorBlockRedirect

--- src/components/edit/InlineEditorBlockRedirect.js
+++ src/components/edit/InlineEditorBlockRedirect.js
@@ -13,36 +13,6 @@ import HtmlInput from './HtmlInput.js'
 import UrlInput from './UrlInput.js'
 
 
-
-function contrastingColor(color) {
-  // source: https://stackoverflow.com/questions/635022/calculating-contrasting-colours-in-javascript
-  // exact answer: https://stackoverflow.com/a/6511606/2387277
-  // example: https://jsfiddle.net/thomasrosen/9njo6t7s/20/
-  
-  return (luma(color) >= 165) ? '000' : 'fff';
-}
-function luma(color) { 
-  // color can be a hex string or an array of RGB values 0-255
-  const rgb = (typeof color === 'string') ? hexToRGBArray(color) : color;
-  return (0.2126 * rgb[0]) + (0.7152 * rgb[1]) + (0.0722 * rgb[2]); // SMPTE C, Rec. 709 weightings
-}
-function hexToRGBArray(color) {
-  if (color.length === 3) {
-    color = color.charAt(0) + color.charAt(0) + color.charAt(1) + color.charAt(1) + color.charAt(2) + color.charAt(2);
-  } else if (color.length !== 6) {
-    throw(new Error('Invalid hex color: ' + color));
-  }
-
-  let rgb = [];
-  for (var i = 0; i <= 2; i++) {
-    rgb[i] = parseInt(color.substr(i * 2, 2), 16);
-  }
-
-  return rgb;
-}
-
-
-
 function InlineEditorBlockRedirectRaw({
   getString,
   block = {},
