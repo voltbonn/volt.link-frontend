66d03c931c9a08a9a74786682d150935c974f63a
thomasrosen <thomas.rosen@me.com>
2022-03-25 20:18:55 +0100

    indention + if statements + choose-type-menu
    - indention
    - smaller if statements ( && instead of ?: )
    - grouped Divider for choose-type-menu into it's if
    - keep the choose-type-menu visible for people (even though it's disabled)

--- src/components/edit/BlockMenu.js
+++ src/components/edit/BlockMenu.js
@@ -190,292 +190,290 @@ function BlockMenu ({
   >
     {({ open }) => (<div>
 
-    <div style={{ height: '8px' }}></div>
+      <div style={{ height: '8px' }}></div>
 
+      {
+        (
+          canEdit
+          && typeof setType === 'function'
+          && Array.isArray(typeOptions)
+          && typeOptions.length > 0
+        )
+        && <>
+          <SubMenu
+            disabled={type === 'person'}
+            parentMenuIsOpen={open}
+            label={<>
+              <ListItemIcon>
+                <RepeatIcon />
+              </ListItemIcon>
+              <Localized id="block_menu_choose_type_label" />
+            </>}
+            header={<Localized id="block_menu_choose_type_label" />}
+          >
             {
-              (
-                && type !== 'person'
-                canEdit
-                && typeof setType === 'function'
-                && Array.isArray(typeOptions)
-                && typeOptions.length > 0
-              )
-              ? <SubMenu
-                  parentMenuIsOpen={open}
-                  label={<>
-                    <ListItemIcon>
-                      <RepeatIcon />
-                    </ListItemIcon>
-                    <Localized id="block_menu_choose_type_label" />
-                  </>}
-                  header={<Localized id="block_menu_choose_type_label" />}
+              typeOptions
+              .map(typeName => (
+                <MenuItem
+                  key={typeName}
+                  selected={typeName === type}
+                  onClick={() => setType(typeName)}
+                  className="roundMenuItem"
                 >
-                  {
-                    typeOptions
-                    .map(typeName => (
-                      <MenuItem
-                        key={typeName}
-                        selected={typeName === type}
-                        onClick={() => setType(typeName)}
-                        className="roundMenuItem"
-                      >
-                        <ListItemIcon>
-                          {typeIcons[typeName]}
-                        </ListItemIcon>
-                        {getString(`block_menu_type_label_${typeName}`, typeName)}
-                      </MenuItem>
-                    ))
-                  }
-                </SubMenu>
-              : null
-            }
-
-            <Divider style={{opacity: 0.2, marginTop:'8px', marginBottom:'8px'}} />
-
-            {
-              (
-                canEdit
-                && typeof setProperty === 'function'
-              )
-              ? <SubMenu
-                  parentMenuIsOpen={open}
-                  label={<>
-                    <ListItemIcon>
-                      <ColorIcon
-                        style={{
-                          color: getColor(color),
-                        }}
-                      />
-                    </ListItemIcon>
-                    <Localized id="block_menu_choose_color_label" />
-                  </>}
-                  header={<Localized id="block_menu_choose_color_label" />}
-                >
-                  <>
-                  {
-                    colorOptionsKeys
-                    .map(thisColor => (
-                      <MenuItem
-                        key={thisColor}
-                        selected={thisColor === 'default' ? color === null : color === thisColor}
-                        onClick={() => setProperty('color', thisColor === 'default' ? null : thisColor)}
-                        className="roundMenuItem"
-                      >
-                        <ListItemIcon>
-                          <ColorSwatchIcon
-                            style={{
-                              color: getColor(thisColor),
-                            }}
-                          />
-                        </ListItemIcon>
-                        {thisColor}
-                      </MenuItem>
-                    ))
-                  }
-
-                  <MenuItem
-                    className="roundMenuItem"
-                    selected={color !== null && !colorOptionsKeys.includes(color)}
-                  >
-                    <ListItemIcon>
-                      <ColorSwatchIcon
-                        style={{
-                          color: renderedColor,
-                        }}
-                      />
-                    </ListItemIcon>
-                    <input
-                      key={colorOptionsKeys.includes(color) ? color : 'custom'}
-                      type="text"
-                      placeholder="#FFFFFF"
-                      defaultValue={color}
-                      onBlur={e => setProperty('color', e.target.value)}
-                      style={{
-                        width: '160px',
-                        margin: '0 calc(-1 * var(--basis)) 0 0',
-                      }}
-                    />
-                  </MenuItem>
-                  </>
-                </SubMenu>
-              : null
-            }
-
-            {
-              (
-                canEdit
-                && typeof setProperty === 'function'
-                && type === 'text'
-              )
-              ? <SubMenu
-                  parentMenuIsOpen={open}
-                  label={<>
-                    <ListItemIcon>
-                      <TextStyleIcon />
-                    </ListItemIcon>
-                    <Localized id="block_menu_choose_text_style_label" />
-                  </>}
-                  header={<Localized id="block_menu_choose_text_style_label" />}
-                >
-                  {
-                    [
-                      // 'h1', // title
-                      'h2', // heading
-                      'h3', // subheading
-                      'body', // body text. This is saved as null.
-                      // 'caption',
-                    ]
-                    .map(thisStyle => (
-                      <MenuItem
-                        key={thisStyle}
-                        selected={thisStyle === 'body' ? text_style === null : text_style === thisStyle}
-                        onClick={() => setProperty('text_style', thisStyle === 'body' ? null : thisStyle)}
-                        className="roundMenuItem"
-                      >
-                        {getString(`block_menu_text_style_label_${thisStyle}`, thisStyle)}
-                      </MenuItem>
-                    ))
-                  }
-                </SubMenu>
-              : null
-            }
-
-            {
-              (
-                canEdit
-                && typeof setProperty === 'function'
-              )
-              && <Divider style={{opacity: 0.2, marginTop:'8px', marginBottom:'8px'}} />
-            }
-
-            {/*
-              addRowBefore
-                ? <MenuItem onClick={handleAddRowBefore}>
-                    <ListItemIcon>
-                      <VerticalAlignTopIcon />
-                    </ListItemIcon>
-                    <Localized id="block_menu_add_before" />
-                  </MenuItem>
-                : null
-            */}
-
-            {/*
-              addRowAfter
-                ? <MenuItem onClick={addRowAfter}>
-                    <ListItemIcon>
-                      <VerticalAlignBottomIcon />
-                    </ListItemIcon>
-                    <Localized id="block_menu_add_after" />
-                  </MenuItem>
-                : null
-            */}
-
-            {
-              canEdit
-              && typeof createBlock === 'function'
-                ? <SubMenu
-                    parentMenuIsOpen={open}
-                    label={<>
-                      <ListItemIcon>
-                        <AddIcon />
-                      </ListItemIcon>
-                      <Localized id="block_menu_create_child_block" />
-                    </>}
-                  >
-                    <AddMenuContent createBlock={createChildBlock} />
-                  </SubMenu>
-                : null
-            }
-
-            {
-              canEdit
-              && typeof setProperty === 'function'
-                ? <MenuItem className="roundMenuItem" onClick={toggleActive}>
-                    <ListItemIcon>
-                      {
-                        active
-                        ? <VisibilityOffIcon />
-                        : <VisibilityIcon />
-                      }
-                    </ListItemIcon>
-                    {
-                      active
-                      ? <Localized id="block_menu_hide" />
-                      : <Localized id="block_menu_show" />
-                    }
-                  </MenuItem>
-                : null
-            }
-
-            {
-              canEdit
-              && typeof onArchivedToggle === 'function'
-              ? <MenuItem className="roundMenuItem" onClick={toggleArchiveBlock}>
                   <ListItemIcon>
-                    {
-                      archived === true
-                      ? <UnarchiveIcon />
-                      : <ArchiveIcon />
-                    }
+                    {typeIcons[typeName]}
                   </ListItemIcon>
-                  {
-                    archived === true
-                    ? <Localized id="block_menu_unarchive" />
-                    : <Localized id="block_menu_archive" />
-                  }
+                  {getString(`block_menu_type_label_${typeName}`, typeName)}
                 </MenuItem>
-              : null
+              ))
             }
+          </SubMenu>
+          <Divider style={{ opacity: 0.2, marginTop: '8px', marginBottom: '8px' }} />
+        </>
+      }
+
+      {
+        (
+          canEdit
+          && typeof setProperty === 'function'
+        )
+        && <SubMenu
+          parentMenuIsOpen={open}
+          label={<>
+            <ListItemIcon>
+              <ColorIcon
+                style={{
+                  color: getColor(color),
+                }}
+              />
+            </ListItemIcon>
+            <Localized id="block_menu_choose_color_label" />
+          </>}
+          header={<Localized id="block_menu_choose_color_label" />}
+        >
+          {
+            colorOptionsKeys
+            .map(thisColor => (
+              <MenuItem
+                key={thisColor}
+                selected={thisColor === 'default' ? color === null : color === thisColor}
+                onClick={() => setProperty('color', thisColor === 'default' ? null : thisColor)}
+                className="roundMenuItem"
+              >
+                <ListItemIcon>
+                  <ColorSwatchIcon
+                    style={{
+                      color: getColor(thisColor),
+                    }}
+                  />
+                </ListItemIcon>
+                {thisColor}
+              </MenuItem>
+            ))
+          }
+
+          <MenuItem
+            className="roundMenuItem"
+            selected={color !== null && !colorOptionsKeys.includes(color)}
+          >
+            <ListItemIcon>
+              <ColorSwatchIcon
+                style={{
+                  color: renderedColor,
+                }}
+              />
+            </ListItemIcon>
+            <input
+              key={colorOptionsKeys.includes(color) ? color : 'custom'}
+              type="text"
+              placeholder="#FFFFFF"
+              defaultValue={color}
+              onBlur={e => setProperty('color', e.target.value)}
+              style={{
+                width: '160px',
+                margin: '0 calc(-1 * var(--basis)) 0 0',
+              }}
+            />
+          </MenuItem>
+        </SubMenu>
+      }
+
+      {
+        (
+          canEdit
+          && typeof setProperty === 'function'
+          && type === 'text'
+        )
+        && <SubMenu
+          parentMenuIsOpen={open}
+          label={<>
+            <ListItemIcon>
+              <TextStyleIcon />
+            </ListItemIcon>
+            <Localized id="block_menu_choose_text_style_label" />
+          </>}
+          header={<Localized id="block_menu_choose_text_style_label" />}
+        >
+          {
+            [
+              // 'h1', // title
+              'h2', // heading
+              'h3', // subheading
+              'body', // body text. This is saved as null.
+              // 'caption',
+            ]
+            .map(thisStyle => (
+              <MenuItem
+                key={thisStyle}
+                selected={thisStyle === 'body' ? text_style === null : text_style === thisStyle}
+                onClick={() => setProperty('text_style', thisStyle === 'body' ? null : thisStyle)}
+                className="roundMenuItem"
+              >
+                {getString(`block_menu_text_style_label_${thisStyle}`, thisStyle)}
+              </MenuItem>
+            ))
+          }
+        </SubMenu>
+      }
+
+      {
+        (
+          canEdit
+          && typeof setProperty === 'function'
+        )
+        && <Divider style={{opacity: 0.2, marginTop:'8px', marginBottom:'8px'}} />
+      }
+
+      {/*
+        addRowBefore
+        && <MenuItem onClick={handleAddRowBefore}>
+          <ListItemIcon>
+            <VerticalAlignTopIcon />
+          </ListItemIcon>
+          <Localized id="block_menu_add_before" />
+        </MenuItem>
+      */}
+
+      {/*
+        addRowAfter
+        && <MenuItem onClick={addRowAfter}>
+          <ListItemIcon>
+            <VerticalAlignBottomIcon />
+          </ListItemIcon>
+          <Localized id="block_menu_add_after" />
+        </MenuItem>
+      */}
+
+      {
+        (
+          canEdit
+          && typeof createBlock === 'function'
+        )
+        && <SubMenu
+          parentMenuIsOpen={open}
+          label={<>
+            <ListItemIcon>
+              <AddIcon />
+            </ListItemIcon>
+            <Localized id="block_menu_create_child_block" />
+          </>}
+        >
+          <AddMenuContent createBlock={createChildBlock} />
+        </SubMenu>
+      }
+
+      {
+        (
+          canEdit
+          && typeof setProperty === 'function'
+        )
+        && <MenuItem className="roundMenuItem" onClick={toggleActive}>
+          <ListItemIcon>
+            {
+              active
+              ? <VisibilityOffIcon />
+              : <VisibilityIcon />
+            }
+          </ListItemIcon>
+          {
+            active
+            ? <Localized id="block_menu_hide" />
+            : <Localized id="block_menu_show" />
+          }
+        </MenuItem>
+      }
+
+      {
+        (
+          canEdit
+          && typeof onArchivedToggle === 'function'
+        )
+        && <MenuItem className="roundMenuItem" onClick={toggleArchiveBlock}>
+          <ListItemIcon>
+            {
+              archived === true
+              ? <UnarchiveIcon />
+              : <ArchiveIcon />
+            }
+          </ListItemIcon>
+          {
+            archived === true
+            ? <Localized id="block_menu_unarchive" />
+            : <Localized id="block_menu_archive" />
+          }
+        </MenuItem>
+      }
             
-            {
-              canEdit
-              && <Divider style={{ opacity: 0.2, marginTop: '8px', marginBottom: '8px' }} />
-            }
+      {
+        canEdit
+        && <Divider style={{ opacity: 0.2, marginTop: '8px', marginBottom: '8px' }} />
+      }
 
-            {
-              (typeof _id === 'string' && _id !== '')
-              && <>
-                <MenuItem component={Link} to={`/${_id}/view`} className="roundMenuItem" style={{ marginTop: '8px' }}>
-                  <ListItemIcon>
-                    <EditIcon />
-                  </ListItemIcon>
-                  <Localized id="view_block" />
-                </MenuItem>
+      {
+        (typeof _id === 'string' && _id !== '')
+        && <>
+          <MenuItem component={Link} to={`/${_id}/view`} className="roundMenuItem" style={{ marginTop: '8px' }}>
+            <ListItemIcon>
+              <EditIcon />
+            </ListItemIcon>
+            <Localized id="view_block" />
+          </MenuItem>
 
-                {
-                  canEdit
-                  && <MenuItem component={Link} to={`/${_id}/edit`} className="roundMenuItem">
-                      <ListItemIcon>
-                        <ViewIcon />
-                      </ListItemIcon>
-                      <Localized id="edit_block" />
-                    </MenuItem>
-                }
+          {
+            canEdit
+            && <MenuItem component={Link} to={`/${_id}/edit`} className="roundMenuItem">
+              <ListItemIcon>
+                <ViewIcon />
+              </ListItemIcon>
+              <Localized id="edit_block" />
+            </MenuItem>
+          }
 
-                <Divider style={{opacity: 0.2, marginTop:'8px', marginBottom:'8px' }} />
-              </>
-            }
+          <Divider style={{opacity: 0.2, marginTop:'8px', marginBottom:'8px' }} />
+        </>
+      }
 
-            {/*
-            <ListItem sx={{ paddingTop: '0', paddingBottom: '0' }}>
-              <ListItemText
-                primary={<Localized id="block_menu_info_created" />}
-                secondary={(metadata.created || '').replace(/[TZ]/g, ' ')}
-              />
-            </ListItem>
-            */}
-            <ListItem sx={{ paddingTop: '0', paddingBottom: '0' }}>
-              <ListItemText
-                primary={<Localized id="block_menu_info_modified" />}
-                secondary={(metadata.modified || '').replace(/[TZ]/g, ' ')}
-              />
-            </ListItem>
-            <ListItem sx={{ paddingTop: '0', paddingBottom: '0' }}>
-              <ListItemText
-                primary={<Localized id="block_menu_info_id" />}
-                secondary={_id}
-              />
-            </ListItem>
+      {/*
+      <ListItem sx={{ paddingTop: '0', paddingBottom: '0' }}>
+        <ListItemText
+          primary={<Localized id="block_menu_info_created" />}
+          secondary={(metadata.created || '').replace(/[TZ]/g, ' ')}
+        />
+      </ListItem>
+      */}
+      <ListItem sx={{ paddingTop: '0', paddingBottom: '0' }}>
+        <ListItemText
+          primary={<Localized id="block_menu_info_modified" />}
+          secondary={(metadata.modified || '').replace(/[TZ]/g, ' ')}
+        />
+      </ListItem>
+      <ListItem sx={{ paddingTop: '0', paddingBottom: '0' }}>
+        <ListItemText
+          primary={<Localized id="block_menu_info_id" />}
+          secondary={_id}
+        />
+      </ListItem>
+
     </div>)}
   </PopoverMenu>
 }
