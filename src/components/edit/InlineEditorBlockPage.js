ad198636157b8f249b0846a047efaa9ff0a8e806
thomasrosen <thomas.rosen@me.com>
2022-09-02 01:40:39 +0200

    feat: InlineEditor for poster
    And now use InlineEditorBlockPage for Person and Poster.

--- src/components/edit/InlineEditorBlockPage.js
+++ src/components/edit/InlineEditorBlockPage.js
@@ -2,12 +2,11 @@ import React from 'react'
 
 import ViewerAuto from '../view/ViewerAuto.js'
 
-export default function InlineEditorBlockPage({
+export default function InlineEditorBlockPerson({
   block = {},
 }) {
   return <div style={{ margin: '0 0 var(--basis) 0' }}>
     <ViewerAuto
-      key={block}
       dragable={true}
       size="line"
       block={block}
