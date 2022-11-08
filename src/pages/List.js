d91c40a7e56b0dbbae1dda2896f99e4005b0fa7a
thomasrosen <thomas.rosen@me.com>
2022-11-08 15:05:56 +0100

    fix: this shouldn't be an object

--- src/pages/List.js
+++ src/pages/List.js
@@ -700,7 +700,7 @@ function List({
           }}
         >
           {/* {getString('block_menu_type_label_plural_' + filters.current.type)} */}
-          {{ title }}
+          {title}
         </h1>
 
         <div className={classes.items}>
